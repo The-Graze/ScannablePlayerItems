a6d9df5fba9dbc11359b52099c6517c5b83d3bbf
The-Graze <reece2004f@gmail.com>
2023-12-17 23:06:13 +0000

    a

--- ScannablePlayerItems/PluginInfo.cs
+++ ScannablePlayerItems/PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.graze.gorillatag.scannableplayeritems";
         public const string Name = "ScannablePlayerItems";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
     }
 }
